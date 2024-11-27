b652e8c154c3d6d3b8e9dbc578ffde93348fa70c
Eka Wahyu <ekawahyu9311@gmail.com>
2024-11-27 17:32:10 +0800

    revisi13

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/run-scenarios', async (req, res) => {
       // Validasi properti skenario
       if (!url || !users || !repeats || !duration) {
         addLogEntry(
-          `Scenario ${index + 1} skipped: Missing required fields.`,
+          `Scenario ${index + 1} skipped: Missing required fields. (url: ${url}, users: ${users}, repeats: ${repeats}, duration: ${duration})`,
           'error'
         );
         results.push({
